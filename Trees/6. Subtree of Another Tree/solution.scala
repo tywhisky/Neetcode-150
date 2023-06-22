fa1e5f9fee8d7c8211f24c3f9e57eaec38758374
tywhisky <fty221@gmail.com>
2023-06-23 01:25:06 +0800

    Update

---
+++ Trees/6. Subtree of Another Tree/solution.scala
@@ -0,0 +1,14 @@
+def isSameTree(p: TreeNode, q: TreeNode): Boolean = {
+  if (p == null & q == null) {
+    true
+  } else if (p != null && q != null) {
+    if (p.value != q.value) {
+      false
+    } else {
+      isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    }
+  } else {
+    // only one of the trees is null
+    false
+  }
+}
